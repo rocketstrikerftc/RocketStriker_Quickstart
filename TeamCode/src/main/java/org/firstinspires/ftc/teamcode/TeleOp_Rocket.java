c3143846e6bb264fc581bffce967a7ac0c4562f2
Emerson Glauber Ribeiro Junior <emersonj482@gmail.com>
2026-04-13 12:19:29 -0300

    adição do codiogo teleoperado a pasta do team code para primeira aula.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Rocket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Rocket.java
@@ -39,7 +39,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @Disabled
 public class TeleOp_Rocket extends LinearOpMode {
 
-    // Declare OpMode members for each of the 4 motors
+    // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
